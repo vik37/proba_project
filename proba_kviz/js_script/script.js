271c0f833d5dc60f9d74144f063dd1b4dc11490f
vik37 <59177404+vik37@users.noreply.github.com>
2020-04-13 08:25:54 +0200

    kviz_josip_broz

---
+++ proba_kviz/js_script/script.js
@@ -0,0 +1,23 @@
+
+
+
+function doAnimation(){
+    let targetAnimationSix = document.getElementById("divDivSix");
+    targetAnimationSix.className = "animSix";
+    let targetAnimationFour = document.getElementById("divDivFour");
+    targetAnimationFour.className = "animFour";
+    let targetAnimationTwo = document.getElementById("divDivTwo");
+    targetAnimationTwo.className = "animTwo";
+    let targetAnimationOne = document.getElementById("divDivOne");
+    targetAnimationOne.className = "animOne";
+    let secondDiv = document.getElementById("thirthDiv");
+    secondDiv.className = "animThirthDiv";
+
+    let btnDiv = document.getElementById("divDivFive");
+    btnDiv.style.display = "none";
+
+    let divQusetion = document.getElementById("divDivThree");
+    divQusetion.style.display = "none";
+ 
+}
+
